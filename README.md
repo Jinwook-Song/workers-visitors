4d14ba89924bdd5410d449d333506b85ab2ad7fa
jinwook song <wlsdnr129@naver.com>
2022-12-16 02:14:15 +0900

    feat: routing, redirect

--- README.md
+++ README.md
@@ -9,10 +9,68 @@
 
 `wrangler login`
 
----
-
 ### Create project
 
 `wrangler init <project_name>`
 
 cloudflare worker == javascript function
+
+---
+
+### routing & redirect
+
+src > home.html
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css"
+    />
+    <title>Visit Count</title>
+  </head>
+  <body class="container">
+    <h1>Visit Counter</h1>
+    <h3>How to use?</h3>
+    <ul>
+      <li>
+        All you have to do is call (GET) this URL:
+        <code> http://127.0.0.1:8787/visit?page=$URL </code>
+      </li>
+      <li>Replace $URL for your website URL</li>
+    </ul>
+  </body>
+</html>
+```
+
+src > index.ts
+
+```tsx
+// @ts-ignore
+import home from './home.html';
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const url = new URL(request.url);
+    if (url.pathname === '/')
+      return new Response(home, {
+        headers: {
+          'Content-Type': 'text/html; charset=utf-8',
+        },
+      });
+    else
+      return new Response(null, {
+        status: 404,
+      });
+  },
+};
+```
--- src/index.ts
+++ src/index.ts
@@ -9,22 +9,35 @@
  */
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  // MY_KV_NAMESPACE: KVNamespace;
+  //
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  // MY_DURABLE_OBJECT: DurableObjectNamespace;
+  //
+  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+  // MY_BUCKET: R2Bucket;
 }
 
+// @ts-ignore
+import home from './home.html';
+
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const url = new URL(request.url);
+    if (url.pathname === '/')
+      return new Response(home, {
+        headers: {
+          'Content-Type': 'text/html; charset=utf-8',
+        },
+      });
+    else
+      return new Response(null, {
+        status: 404,
+      });
+  },
 };
